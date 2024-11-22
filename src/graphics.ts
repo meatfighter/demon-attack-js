7d27543c9f3db13e87097bfa1d1e3b2e1d84d541
Michael Birken <o__1@hotmail.com>
2024-11-22 15:37:02 -0500

    update

--- src/graphics.ts
+++ src/graphics.ts
@@ -7,3 +7,22 @@ export enum PhysicalDimensions {
     WIDTH = 4,
     HEIGHT = 3,
 }
+
+export const PALETTE = extractPalette();
+
+function extractPalette(): Uint8ClampedArray {
+    const binStr = atob('AAAAPz8+ZGRjhISDoqKhurq50tLR6urpPT0AXl4Ke3sVmZkgtLQqzc005uY+/f1IcSMAhj0LmVcYrW8mvYYyzZs+3LBJ6s'
+        + 'JUhhUAmi8OrkgewGEv0Xc+4I1N76Jb/bVoigAAnhMSsSgnwj080lFQ4mRj73V0/YaFeQBYjRJuoCeEsTuYwE6q0GG83XHM6oLcRQB4XRKPci'
+        + 'ekiDu5m07KrmHcv3Hs0IL7DgCFKROZQyitXT2/dFHQi2TfoXXutYb7AACKEhOdJCiwNz3BSVHRWmTganXueYb7ABV9EjGTJEynN2e7SYDMWp'
+        + 'fdaq7tecL7ACdYEkV0JGKNN36nSZe+WrDUasfoed37ADUmEldCJHZdN5V2SbGOWsylauW7ef3PADkAE1sSKHknPZc8UbNQZM1jdeZ0hv2FDj'
+        + 'IAK1QRR3MjY5M2fbBIlctZreVpwv14Jy4ARU4PYmshfogzl6NDsLxTx9Ri3epwPSMAXkINe18dmXsttJY7za9K5sdX/d1k');       
+    const len = binStr.length;
+    const palette = new Uint8ClampedArray(len);    
+    for (let i = 0; i < len; ++i) {
+        palette[i] = binStr.charCodeAt(i);
+    }
+    return palette;
+}
+
+
+
