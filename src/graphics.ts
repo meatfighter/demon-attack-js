32d5afd36bfbf80f46b7bf462286538ccbab60be
Michael Birken <o__1@hotmail.com>
2024-11-23 10:25:13 -0500

    update

--- src/graphics.ts
+++ src/graphics.ts
@@ -1,25 +1,33 @@
+class Color {
+    constructor(public r: number, public g: number, public b: number) {        
+    }
+}
+
 export enum Resolution {
     WIDTH = 160,
     HEIGHT = 192,
 }
 
+// export enum PhysicalDimensions {
+//     WIDTH = 4,
+//     HEIGHT = 3,
+// }
+
 export enum PhysicalDimensions {
-    WIDTH = 4,
-    HEIGHT = 3,
+    WIDTH = 320,
+    HEIGHT = 192,
 }
 
-export const PALETTE = extractPalette();
-
-function extractPalette(): Uint8ClampedArray {
+function extractPalette(): Color[] {
+    const palette = new Array<Color>(256);
     const binStr = atob('AAAAPz8+ZGRjhISDoqKhurq50tLR6urpPT0AXl4Ke3sVmZkgtLQqzc005uY+/f1IcSMAhj0LmVcYrW8mvYYyzZs+3LBJ6s'
         + 'JUhhUAmi8OrkgewGEv0Xc+4I1N76Jb/bVoigAAnhMSsSgnwj080lFQ4mRj73V0/YaFeQBYjRJuoCeEsTuYwE6q0GG83XHM6oLcRQB4XRKPci'
         + 'ekiDu5m07KrmHcv3Hs0IL7DgCFKROZQyitXT2/dFHQi2TfoXXutYb7AACKEhOdJCiwNz3BSVHRWmTganXueYb7ABV9EjGTJEynN2e7SYDMWp'
         + 'fdaq7tecL7ACdYEkV0JGKNN36nSZe+WrDUasfoed37ADUmEldCJHZdN5V2SbGOWsylauW7ef3PADkAE1sSKHknPZc8UbNQZM1jdeZ0hv2FDj'
-        + 'IAK1QRR3MjY5M2fbBIlctZreVpwv14Jy4ARU4PYmshfogzl6NDsLxTx9Ri3epwPSMAXkINe18dmXsttJY7za9K5sdX/d1k');       
-    const len = binStr.length;
-    const palette = new Uint8ClampedArray(len);    
-    for (let i = 0; i < len; ++i) {
-        palette[i] = binStr.charCodeAt(i);
+        + 'IAK1QRR3MjY5M2fbBIlctZreVpwv14Jy4ARU4PYmshfogzl6NDsLxTx9Ri3epwPSMAXkINe18dmXsttJY7za9K5sdX/d1k');         
+    for (let i = 0x00; i <= 0xFF; ++i) {       
+        const j = 3 * (i >> 1);
+        palette[i] = new Color(binStr.charCodeAt(j), binStr.charCodeAt(j + 1), binStr.charCodeAt(j + 2));
     }
     return palette;
 }
